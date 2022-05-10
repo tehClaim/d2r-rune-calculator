faf7507a226a81c50c7633fe38bd96c49158afc8
Kirill Shashkov <exclaimz.dev@gmail.com>
2022-05-10 21:54:53 +0300

    feat: add dummy components for inventory and result

---
+++ src/components/inventory/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Inventory: React.FC = () => (
+    <>inventory rune list</>
+);
+
+export default Inventory;
---
+++ src/components/result/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Result: React.FC = () => (
+    <>must contain 2 components: cacl config (select end rune) and list of required mats/runes</>
+);
+
+export default Result;
